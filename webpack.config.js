f552e74577bebbfdd94ec34aaa402d4d86cb76e8
Raksha <rakshapawar10.sae@gmail.com>
2020-09-24 12:32:28 +0530

    Necessary deployment changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,62 @@
 const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+module.exports = (env) => {
+  const isProduction = env === "production";
+
+  return {
+    entry: path.resolve(__dirname, "src", "app.js"),
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "public", "dist"),
+    },
+    mode: "development",
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: "./public",
+      historyApiFallback: true,
+      publicPath: "/dist/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+              plugins: ["@babel/plugin-proposal-class-properties"],
+            },
+          },
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                url: false,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "styles.css",
+      }),
     ],
-  },
-
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-  },
+  };
 };
